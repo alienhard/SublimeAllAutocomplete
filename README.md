84fcba525d414eb87d9c45f7e6cfc655b835358b
Adrian Lienhard <adrian@scribd.com>
2013-02-23 14:49:29 -0800

    Sublime Text version 3 compatibility and quality improvements

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
-All Autocomplete Sublime Text 2
+All Autocomplete Sublime Text
 ===========================================================
 
-Extends the default autocomplete to find matches in all open files of the current window.
+Extends the default autocomplete to find matches in all open files.
 
 By default Sublime only considers words found in the current file.
 
@@ -9,7 +9,7 @@ By default Sublime only considers words found in the current file.
 Install
 -------
 
-If you have Package Control installed in Sublime Text 2 just press ctrl+shift+p (Windows, Linux) or cmd+shift+p (OS X) to open the Command Pallete.
+If you have Package Control installed in Sublime just press ctrl+shift+p (Windows, Linux) or cmd+shift+p (OS X) to open the Command Pallete.
 Start typing 'install' to select 'Package Control: Install Package', then search for AllAutocomplete and select it. That's it.
 
 You can also install this package manually by entering the Packages directory of Sublime Text 2 and issuing on a terminal:
--- all_views_completions.py
+++ all_views_completions.py
@@ -1,6 +1,5 @@
-# Extend Sublime Text 2 autocompletion to find matches in all open
-# files of the current window. By default, Sublime only considers
-# words from the current file.
+# Extends Sublime Text autocompletion to find matches in all open
+# files. By default, Sublime only considers words from the current file.
 
 import sublime_plugin
 import sublime
@@ -8,40 +7,56 @@ import re
 
 
 # limits to prevent bogging down the system
-MAX_VIEWS = 25
-MAX_WORDS = 10
+MAX_VIEWS = 20
+MAX_WORDS_PER_VIEW = 100
 MIN_WORD_SIZE = 3
 
 
 class AllAutocomplete(sublime_plugin.EventListener):
 
     def on_query_completions(self, view, prefix, locations):
-        words = set()
-        views = [v for v in sublime.active_window().views()]
+        words = []
 
-        # limit number of views but include the current view
-        views = set(views[0:MAX_VIEWS])
-        views.add(view)
+        # Limit number of views but always include the active view. This
+        # view goes first to prioritize matches close to cursor position.
+        other_views = [v for v in sublime.active_window().views() if v.id != view.id]
+        views = [view] + other_views
+        views = views[0:MAX_VIEWS]
 
         for v in views:
-            view_words = v.extract_completions(prefix)
-            view_words = view_words[0:MAX_WORDS]
+            if len(locations) > 0 and v.id == view.id:
+                view_words = v.extract_completions(prefix, locations[0])
+            else:
+                view_words = v.extract_completions(prefix)
+            view_words = view_words[0:MAX_WORDS_PER_VIEW]
             view_words = fix_truncation(v, view_words)
-            words.update(view_words)
+            words += view_words
+
+        words = without_duplicates(words)
         matches = [(w, w) for w in words]
         return matches
+
+
+# keeps first instance of every word and retains the original order
+# (n^2 but should not be a problem as len(words) <= MAX_VIEWS*MAX_WORDS_PER_VIEW)
+def without_duplicates(words):
+    result = []
+    for w in words:
+        if w not in result:
+            result.append(w)
+    return result
 
 
 # Ugly workaround for truncation bug in Sublime when using view.extract_completions()
 # in some types of files.
 def fix_truncation(view, words):
-    fixed_words = set()
-    words_to_fix = set()
+    fixed_words = []
+    words_to_fix = []
     for w in words:
         if len(w) >= MIN_WORD_SIZE:
-            words_to_fix.add(w)
+            words_to_fix.append(w)
         else:
-            fixed_words.add(w)
+            fixed_words.append(w)
 
     for w in words_to_fix:
         #The word is truncated if and only if it cannot be found with a word boundary before and after
@@ -54,12 +69,12 @@ def fix_truncation(view, words):
             extended_words = []
             view.find_all(r'\b' + re.escape(w) + r'\w\b', 0, "$0", extended_words)
             if len(extended_words) > 0:
-                fixed_words.update(extended_words)
+                fixed_words += extended_words
             else:
                 # to compensate for the missing match problem mentioned above, just
                 # use the old word if we didn't find any extended matches
-                fixed_words.add(w)
+                fixed_words.append(w)
         else:
             #Pass through non-truncated words
-            fixed_words.add(w)
+            fixed_words.append(w)
     return fixed_words
